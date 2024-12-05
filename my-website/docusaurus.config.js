569e5229133982734455452ed92d07f4482ea9b2
dolgikh <dolgikht17@gmail.com>
2024-12-05 23:49:22 +0300

    v2.4

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -25,7 +25,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'dolgikh', // Usually your GitHub org/user name.
+  organizationName: 'dolgikh17', // Usually your GitHub org/user name.
   projectName: 'dolgikh_docs', // Usually your repo name.
 
   onBrokenLinks: 'warn',
